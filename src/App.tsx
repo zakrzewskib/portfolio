1269f51ffe00b38e5301a3a38e0253ecafa82287
zakrzewskib <barzak200@gmail.com>
2025-02-26 16:10:17 +0100

    feat: Add responsive font sizes with MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Container } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 import { useLocation } from 'react-router';
 import { Footer, NavBar } from './components';
@@ -8,18 +8,20 @@ import { MAX_WIDTH } from './constants';
 import { Router } from './router';
 import { store } from './store/store';
 
-const darkTheme = createTheme({
+const themeWithDarkMode = createTheme({
   colorSchemes: {
     dark: true,
   },
 });
+
+const themeWithResponsiveFontSizes = responsiveFontSizes(themeWithDarkMode);
 
 function App() {
   const { pathname } = useLocation();
 
   return (
     <Provider store={store}>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={themeWithResponsiveFontSizes}>
         <CssBaseline /> {/* enable dark mode for the background */}
         <NavBar maxWidth={MAX_WIDTH} />
         <Container maxWidth={MAX_WIDTH} component='main'>
