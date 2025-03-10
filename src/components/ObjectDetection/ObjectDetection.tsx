c2a7a0ff0a20e3cefe38e4a54e50cd1d8bed5658
zakrzewskib <barzak200@gmail.com>
2025-03-10 14:33:22 +0100

    reformat: Reformat object detection

--- src/components/ObjectDetection/ObjectDetection.tsx
+++ src/components/ObjectDetection/ObjectDetection.tsx
@@ -1,44 +1,41 @@
 import { Box } from '@mui/material';
 import * as cocossd from '@tensorflow-models/coco-ssd';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import cat from '../../assets/images/cat-eating.jpg';
 import { drawRect } from './canvasUtils';
 
 // todo: Add loading state
+
 export const ObjectDetection = () => {
   const imgRef = useRef<HTMLImageElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [, setModel] = useState<cocossd.ObjectDetection>();
 
   useEffect(() => {
     cocossd.load().then((model) => {
-      const img = imgRef.current!;
-      const canvas = canvasRef.current!;
-
-      model.detect(img).then((predictions) => {
-        img.width = canvas.width = 640;
-        img.height = canvas.height = 480;
-
-        const ctx = canvas.getContext('2d')!;
-        drawRect(ctx, predictions);
-      });
+      setModel(model);
+      detectObjectsAndDrawRects(model);
     });
   }, []);
 
-  return (
-    <Box sx={{ position: 'relative', width: '640px', height: '480px' }}>
-      <img
-        src={cat}
-        alt='Cat eating from a bowl'
-        ref={imgRef}
-        width={640}
-        height={480}
-        style={{ position: 'absolute', top: 0, left: 0, zIndex: 1, textAlign: 'center' }}
-      />
+  const detectObjectsAndDrawRects = (model: cocossd.ObjectDetection) => {
+    const img = imgRef.current!;
+    const canvas = canvasRef.current!;
 
-      <canvas
-        ref={canvasRef}
-        style={{ width: 640, height: 480, position: 'absolute', top: 0, left: 0, zIndex: 2, textAlign: 'center' }}
-      />
+    model.detect(img).then((predictions) => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      const ctx = canvas.getContext('2d')!;
+      drawRect(ctx, predictions);
+    });
+  };
+
+  return (
+    <Box sx={{ position: 'relative' }}>
+      <img src={cat} alt='Cat eating from a bowl' ref={imgRef} />
+
+      <canvas ref={canvasRef} style={{ position: 'absolute', top: 0, left: 0, zIndex: 2, textAlign: 'center' }} />
     </Box>
   );
 };
--- src/components/ObjectDetection/canvasUtils.tsx
+++ src/components/ObjectDetection/canvasUtils.tsx
@@ -5,6 +5,7 @@ export const drawRect = (ctx: CanvasRenderingContext2D, detections: DetectedObje
     const [x, y, width, height] = prediction['bbox'];
     const text = prediction['class'];
 
+    // todo: Generate 'better' colors
     const color = Math.floor(Math.random() * 16777215).toString(16);
 
     ctx.beginPath();
