feac1ef2d457f5ac9373f280a6f97903a18caa4e
zakrzewskib <barzak200@gmail.com>
2025-03-10 15:29:38 +0100

    docs: Add info text

--- src/components/ObjectDetection/ObjectDetectionInfo.tsx
+++ src/components/ObjectDetection/ObjectDetectionInfo.tsx
@@ -1,6 +1,7 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Link, Typography } from '@mui/material';
 import '@tensorflow/tfjs-backend-cpu';
 import '@tensorflow/tfjs-backend-webgl';
+import { Skill } from '../ui';
 import { ObjectDetection } from './ObjectDetection';
 
 export const ObjectDetectionInfo = () => {
@@ -18,7 +19,18 @@ export const ObjectDetectionInfo = () => {
         }}
       >
         <Box className='maxWidthForText' sx={{ mb: 2, flex: 1 }}>
-          This text will be about Tensorflow.js
+          I wanted to try using Machine Learning and I found out that there is a JavaScript version of ML framework -
+          Tensorflow.
+          <Box sx={{ display: 'flex', flexWrap: 'true', alignItems: 'center', gap: 1, mb: 1, mt: 1 }}>
+            <Skill>#Tensorflow.js</Skill>
+            <Skill>#Object recognition</Skill>
+          </Box>
+          I decided to use already pre-trained model (
+          <Link target='_blank' href='https://www.tensorflow.org/js/models'>
+            tensorflow.org/js/models
+          </Link>
+          ), which meant I could use it 'out of the box' instead of training it myself (like in classic ML I guess).
+          These models are not perfect, so don't be suprised if you upload some image and get wrong or no results :).
         </Box>
 
         <Box>
