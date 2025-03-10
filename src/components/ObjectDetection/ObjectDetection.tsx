e3a852f2f809a9e8f3fc3313f29c8ab83573df82
zakrzewskib <barzak200@gmail.com>
2025-03-10 14:05:21 +0100

    feat: Draw rectangles on image with predictions

--- src/components/ObjectDetection/ObjectDetection.tsx
+++ src/components/ObjectDetection/ObjectDetection.tsx
@@ -1,19 +1,44 @@
+import { Box } from '@mui/material';
 import * as cocossd from '@tensorflow-models/coco-ssd';
-import cat from '../../assets/images/cat-eating.jpg';
-
 import { useEffect, useRef } from 'react';
+import cat from '../../assets/images/cat-eating.jpg';
+import { drawRect } from './canvasUtils';
 
 // todo: Add loading state
 export const ObjectDetection = () => {
   const imgRef = useRef<HTMLImageElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    cocossd.load().then((model) =>
-      model.detect(imgRef.current!).then((predictions) => {
-        console.log('Predictions: ', predictions);
-      })
-    );
+    cocossd.load().then((model) => {
+      const img = imgRef.current!;
+      const canvas = canvasRef.current!;
+
+      model.detect(img).then((predictions) => {
+        img.width = canvas.width = 640;
+        img.height = canvas.height = 480;
+
+        const ctx = canvas.getContext('2d')!;
+        drawRect(ctx, predictions);
+      });
+    });
   }, []);
 
-  return <img id='img' src={cat} alt='Cat eating from a bowl' ref={imgRef} />;
+  return (
+    <Box sx={{ position: 'relative', width: '640px', height: '480px' }}>
+      <img
+        src={cat}
+        alt='Cat eating from a bowl'
+        ref={imgRef}
+        width={640}
+        height={480}
+        style={{ position: 'absolute', top: 0, left: 0, zIndex: 1, textAlign: 'center' }}
+      />
+
+      <canvas
+        ref={canvasRef}
+        style={{ width: 640, height: 480, position: 'absolute', top: 0, left: 0, zIndex: 2, textAlign: 'center' }}
+      />
+    </Box>
+  );
 };
---
+++ src/components/ObjectDetection/canvasUtils.tsx
@@ -0,0 +1,21 @@
+import { DetectedObject } from '@tensorflow-models/coco-ssd';
+
+export const drawRect = (ctx: CanvasRenderingContext2D, detections: DetectedObject[]) => {
+  detections.forEach((prediction) => {
+    const [x, y, width, height] = prediction['bbox'];
+    const text = prediction['class'];
+
+    const color = Math.floor(Math.random() * 16777215).toString(16);
+
+    ctx.beginPath();
+
+    ctx.font = '18px Arial';
+    ctx.fillStyle = '#' + color;
+    ctx.fillText(text, x, y);
+
+    ctx.strokeStyle = '#' + color;
+    ctx.rect(x, y, width, height);
+
+    ctx.stroke();
+  });
+};
